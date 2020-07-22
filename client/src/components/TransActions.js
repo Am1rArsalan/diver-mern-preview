e26c786cba89a55bddc3209478f61d8d8a94d181
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2020-07-22 16:00:05 +0430

    working on the TransActions and adding redux

--- client/src/components/TransActions.js
+++ client/src/components/TransActions.js
@@ -1,49 +1,18 @@
-import React, { useState, useEffect } from "react";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
+import React, { useState} from "react";
+import { useTheme } from "@material-ui/core/styles";
 import { List, useMediaQuery } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
 import SuspenseTransactions from "./UI/SuspenseTransactions";
 import useScrollPos from "../hooks/useScrollPos";
 import TransAction from "./TransAction";
+import useTransActionStyles from './UI/styles/TransActionStyles';
+import useTransActions from '../hooks/useTransActions';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  demo: {
-    backgroundColor: theme.palette.background.paper,
-    marginBottom: 40,
-  },
-  title: {
-    margin: theme.spacing(4, 0, 2),
-  },
-}));
-
-function generateContacts() {
-  let conatcts = [0, 1, 2, 3, 4, 5, 6, 7, 8].map((value) => <TransAction />);
-  return new Promise((res, rej) => {
-    setTimeout(() => {
-      res(conatcts);
-    }, 1000);
-  });
-}
-
-function useContacts() {
-  const [conatcts, setContacts] = useState([]);
-  useEffect(() => {
-    fetchData();
-  }, []);
-  const fetchData = async () => {
-    const res = await generateContacts();
-    setContacts(res);
-  };
-  return conatcts;
-}
 
 export default function () {
-  const classes = useStyles();
+  const classes = useTransActionStyles();
   const [dense, setDense] = useState(false);
-  const transActions = useContacts();
+  const state = useTransActions();
   const scrollPos = useScrollPos();
   const theme = useTheme();
   const match = useMediaQuery(theme.breakpoints.down("sm"));
@@ -58,12 +27,14 @@ export default function () {
               style={{ marginTop: scrollPos !== 0 && match ? 100 : "auto" }}
             >
               <SuspenseTransactions
-                condition={transActions.length > 0}
+                condition={state.data.length > 0}
                 placeholder={Placeholder}
                 multiplier={20}
                 initialDelay={200}
               >
-                {transActions}
+                {state.data.map((item, i) => {
+                  return <TransAction data={item} key={i} />;
+                })}
               </SuspenseTransactions>
             </List>
           </div>
